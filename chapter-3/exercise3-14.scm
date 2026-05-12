33b034108eb1e4fe5f3679165befe301e9720248
MPins <pinsdorf@proton.me>
2026-05-11 22:19:08 -0300

    exercise 3.14

---
+++ chapter-3/exercise3-14.scm
@@ -0,0 +1,17 @@
+;guile 3.0.9
+;Exercise 3.14
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+(define v (list 'a 'b 'c 'd))
+
+(define w (mystery v))
+
+(format #t "w = ~a\n" w)
+(format #t "v = ~a\n" v)
