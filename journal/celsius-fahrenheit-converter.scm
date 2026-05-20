807fa94ad51967f14b8612298a2981e44972b9c9
MPins <pinsdorf@proton.me>
2026-05-20 17:28:13 -0300

    celsius-fahrenheit-converter

---
+++ journal/celsius-fahrenheit-converter.scm
@@ -0,0 +1,30 @@
+(load "../journal/propagation-of-constrains.scm")
+(define (celsius-fahrenheit-converter c f)
+    (let ((u (make-connector))
+          (v (make-connector))
+          (w (make-connector))
+          (x (make-connector))
+          (y (make-connector)))
+        (multiplier c w u)
+        (multiplier v x u)
+        (adder v y f)
+        (constant 9 w)
+        (constant 5 x)
+        (constant 32 y)
+        'ok))
+
+(define C (make-connector))
+(define F (make-connector))
+
+(celsius-fahrenheit-converter C F)
+
+(probe "Celsius temp" C)
+(probe "Fahrenheit temp" F)
+
+(set-value! C 25 'user)
+(display "\n")
+
+(forget-value! C 'user)
+(display "\n")
+
+(set-value! F 212 'user)
