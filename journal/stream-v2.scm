d532c35b93075801ffe8970786dd3422fc7668b2
MPins <pinsdorf@proton.me>
2026-05-26 04:33:32 -0300

    stream-v2

---
+++ journal/stream-v2.scm
@@ -0,0 +1,42 @@
+(define-syntax cons-stream
+    (syntax-rules ()
+        ((_ a b) (cons a (delay b)))))
+
+(define (stream-null? s)
+    (null? s))
+
+(define the-empty-stream '())
+
+(define (stream-enumerate-interval low high)
+    (if (> low high)
+        the-empty-stream
+    (cons-stream
+        low
+        (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-ref s n)
+    (if (= n 0)
+        (stream-car s)
+    (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+    (if (stream-null? (car argstreams))
+        the-empty-stream
+    (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+            (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-for-each proc s)
+    (if (stream-null? s)
+        'done
+    (begin (proc (stream-car s))
+        (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+    (stream-for-each display-line s))
+
+(define (display-line x) (newline) (display x))
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
