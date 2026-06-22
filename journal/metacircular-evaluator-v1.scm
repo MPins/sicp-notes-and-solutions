744677ffa2423f6474d03fa0f4eba41db78787f1
MPins <pinsdorf@proton.me>
2026-06-21 21:41:21 -0300

    metacircular evaluator

--- journal/metacircular-evaluator-v1.scm
+++ journal/metacircular-evaluator-v1.scm
@@ -43,12 +43,10 @@
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
-      (let ((first-value
-             (eval (first-operand exps) env)))
-        (cons first-value
-              (list-of-values
-               (rest-operands exps)
-               env)))))
+      (cons (eval (first-operand exps) env)
+            (list-of-values
+              (rest-operands exps)
+              env))))
 
 (define (eval-if exp env)
   (if (true? (eval (if-predicate exp) env))
